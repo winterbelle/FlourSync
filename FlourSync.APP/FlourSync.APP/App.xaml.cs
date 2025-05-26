c1cc10e13cd06c36d206750842159f81c1270140
Blanca <blanca.altamirano0318@gmail.com>
2025-05-26 15:14:13 -0400

    Skeleton

---
+++ FlourSync.APP/FlourSync.APP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FlourSync.APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FlourSync.APP/FlourSync.APP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FlourSync.APP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
